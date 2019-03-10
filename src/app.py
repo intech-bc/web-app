9cedb5de5b9f464dba859eff3adaa8639e561435
BobbyHen <45887234+BobbyHen@users.noreply.github.com>
2019-03-09 22:58:20 -0500

    Modified app.py file to allow interaction with MongoDB. Created connectionString.txt
    Imported PyMongo driver to app.py file to allow web app to interact with Mongo databases. Store quotes pulled from the Ron Swanson API in database. Created connectionString.txt to store connection string to be read by app.py file.

--- src/app.py
+++ src/app.py
@@ -1,12 +1,22 @@
 import ron
+import pymongo
+
+from pymongo import MongoClient
 from flask import Flask
+
+inFile = open("connectionString.txt", "r")
+client = pymongo.MongoClient(inFile.readline())
+db = client.ronSwansonQuotes
 
 website = Flask(__name__)
 
 @website.route("/")
 def index():
     quote = ron.get_quote()
+    data = {'quote':quote[0]}
+    db.quotes.insert_one(data)
     return "<html><body><p>" + quote[0] + "</p></body></html>"
+
 
 @website.route("/<num>")
 def index_multi(num):
