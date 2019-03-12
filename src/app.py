189eaf38521a74db8122153b20f44de1f8d741ba
Jorge Ortiz <ecnavda@gmail.com>
2019-03-11 20:06:31 -0400

    Added comments to the code. Refactored the connection to the Mongo DB. Included a .gitignore to ignore the .idea folder created by JetBrains IDEs.

--- src/app.py
+++ src/app.py
@@ -1,25 +1,34 @@
 import ron
-import pymongo
-
 from pymongo import MongoClient
 from flask import Flask
-
-inFile = open("connectionString.txt", "r")
-client = pymongo.MongoClient(inFile.readline())
-db = client.ronSwansonQuotes
-
+# creating an instance of the Flask class using the special __name__ variable
 website = Flask(__name__)
 
+# Connecting to the database
+mongoURI = ""
+with open("connectionString.txt", "r") as file:
+    mongoURI = file.readline()
+client = MongoClient(mongoURI)
+# The ronSwansonQuotes database is created if it doesn't exist
+db = client.ronSwansonQuotes
+
+
+# Flask uses function decorators to trigger functions based on URL accessed
 @website.route("/")
 def index():
+    # quote is a list
     quote = ron.get_quote()
-    data = {'quote':quote[0]}
+    data = {
+        "quote": quote[0],
+    }
+    # The quotes collection is created if it doesn't exist
     db.quotes.insert_one(data)
-    return "<html><body><p>" + quote[0] + "</p></body></html>"
+    return "<html><body><p>" + data["quote"] + "</p></body></html>"
 
 
 @website.route("/<num>")
 def index_multi(num):
+    # quotes is a list
     quotes = ron.get_quotes(num)
     html = "<html><body>"
     for x in quotes:
